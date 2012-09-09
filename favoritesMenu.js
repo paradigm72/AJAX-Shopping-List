dacb4ce5f06190a735673a1df5db21801136df52
Paul Romine <promine@gmail.com>
2012-09-08 23:12:31 -0500

    start refactoring favorites menu (not functional yet)

--- favoritesMenu.js
+++ favoritesMenu.js
@@ -10,67 +10,84 @@
 --- *pmr 8/18/12 - rewritten to be an inline popup instead of a separate UI element
 ---------------------------*/
 
-window.onload = function () {
-	initalizeFavoritesMenuHandlers();
+//create the namespace if not already created
+var com;
+if (!com) {
+	com = {};
+}
+if (!com.letxbe) {
+	com.letxbe = {};
 }
 
 
-var initalizeFavoritesMenuHandlers = function () {
-	var textBox = document.getElementById('newItem');
-	var inlineFavorites = document.getElementById('inlineFavorites');
-	var inputRow = document.getElementById('inputRow');
+com.letxbe.FavoritesMenu = function () {
+	var textBox;
+	var inlineFavorites;
+	var inputRow;
+	var FavoritesArray;
+	
+	/*Initialize the event handlers*/
+	function assignFavoritesEventHandlers() {
+		textBox = document.getElementById('newItem');
+		inlineFavorites = document.getElementById('inlineFavorites');
+		inputRow = document.getElementById('inputRow');
+		FavoritesArray = [];
+	
+	
+		textBox.onkeydown = updateDisplayList;
+		textBox.onkeypress = updateDisplayList;
+		textBox.onkeyup = updateDisplayList;
+		inlineFavorites.onclick = favoriteEntryClick;
+	}	
 	
 	/*When key presses are detected, refresh the text contents of the list*/
-	var updateDisplayList = function () {
-		return function () {
-			inlineFavorites.innerHTML = "";
-			var searchText = textBox.value;
-			if (searchText.length == 0) { return true; }
-			var re = new RegExp(searchText + ".");
-			for (i=0; i < FavoritesArray.length; i += 1) {
-				var thisFavorite = FavoritesArray[i].childNodes[0].nodeValue;
-				if (thisFavorite.match(re)) {
-					inlineFavorites.innerHTML = FavoritesArray[i].childNodes[0].nodeValue;
-				}
+	function updateDisplayList() {
+		inlineFavorites.innerHTML = "";
+		var searchText = textBox.value;
+		if (searchText.length == 0) { return true; }
+		var re = new RegExp(searchText + ".");
+		for (i=0; i < FavoritesArray.length; i += 1) {
+			var thisFavorite = FavoritesArray[i].childNodes[0].nodeValue;
+			if (thisFavorite.match(re)) {
+				inlineFavorites.innerHTML = FavoritesArray[i].childNodes[0].nodeValue;
 			}
-		};
-	};	
-	textBox.onkeydown = updateDisplayList();
-	textBox.onkeypress = updateDisplayList();
-	textBox.onkeyup = updateDisplayList();
-	
-	
-	/*When the cursor/focus leaves the text box, hide the list*/
-	var hideList = function () {
-		return function () {
-			inlineFavorites.innerHTML = ""; 
-		};		
-	};	
-	//textBox.onblur = hideList();
-	
+		}
+	}
 	
 	/*When the user clicks on an entry in the favorites div, add that to the list*/
-	var favoriteEntryClick = function () {
-		return function () {
-			var itemToAdd = inlineFavorites.innerHTML;
-			hideList()(); 
-			addItemToList(itemToAdd);			
-		};
-	};
-	inlineFavorites.onclick = favoriteEntryClick();
-};
+	function favoriteEntryClick() {
+		var itemToAdd = inlineFavorites.innerHTML;
+		hideList()(); 
+		addItemToList(itemToAdd);			
+	}
+	
+	/*When the cursor/focus leaves the text box, hide the list*/
+	function hideList() {
+		inlineFavorites.innerHTML = ""; 
+	}
+	
+	/*Load the favorite items from the server into an array*/
+	function loadFavoritesList() {
+		var thePage = 'server/retrieveFavorites.php';
+		myRand = parseInt(Math.random()*999999999);
+		var theURL = thePage + "?rand=" + myRand;
+		myReq.open("GET", theURL, true);
+		myReq.onreadystatechange = retrieveFavoritesHTTPResponse;
+		myReq.send(null);
+	}	
+	
+	return {
+		Initialize: function() { assignFavoritesEventHandlers(); },
+		UpdateDisplayList: function() { updateDisplayList(); },
+		FavoriteEntryClick: function() { favoriteEntryClick(); },
+		HideList: function() { hideList(); },
+		LoadFavoritesList: function() { loadFavoritesList(); }
+	}
+}();
 
-var FavoritesArray = [];
 
-/*Note: this is called by retrieveListHTTPResponse*/
-var loadFavoritesList = function () {
-	var thePage = 'server/retrieveFavorites.php';
-	myRand = parseInt(Math.random()*999999999);
-	var theURL = thePage + "?rand=" + myRand;
-	myReq.open("GET", theURL, true);
-	myReq.onreadystatechange = retrieveFavoritesHTTPResponse;
-	myReq.send(null);		
-}
+com.letxbe.FavoritesMenu.Initialize();
+
 
 var retrieveFavoritesHTTPResponse = function () {
 	if (myReq.readyState == 4) {
