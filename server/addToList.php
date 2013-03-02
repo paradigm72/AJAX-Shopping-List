125eee0c78195e507b953414efc02aee235329d0
Paul Romine <promine@gmail.com>
2013-03-02 09:11:45 -0600

    strip the slashes added to the POST by PHP because of "magic quotes"

--- server/addToList.php
+++ server/addToList.php
@@ -11,12 +11,33 @@
 -  *pmr 08/18/12 use a more sane delimiter
 --------------------------->
 
-<?php	
+<?php
+
+	//error_reporting(E_ALL | E_STRICT);
+	//ini_set('display_errors', 'On');
+	
+	//Need to turn off "magic quotes" (my hosting runs on PHP 5.3 or earlier, apparently)
+	if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+    }
+
 	////////////////////////////
 	/// ADD ITEM TO THIS SHOPPING LIST 
 	////////////////////////////
 	//get the item name
-	$itemName=$_POST['itemName'];
+	$itemName=$_POST['itemName'];	
 	
 	//strip HTML characters
 	$itemName=strip_tags($itemName);
