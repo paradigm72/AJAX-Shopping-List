032d11f8e14ec273f45256307b71de43312984ee
Paul Romine <promine@gmail.com>
2013-03-17 21:41:55 -0500

    functioning list with gotten/remove/add

--- index.php
+++ index.php
@@ -33,8 +33,6 @@
 <script src="favoritesMenu.js" type="text/javascript"></script>
 
 <!-- Bootstrap/Angular-->
-<script src="js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js"></script>
 <script src="shoppingListNg.js"></script>
 
@@ -55,25 +53,44 @@ retrieveShoppingList();
 <div ng-controller="ShoppingListCtrl">
 <table class="items">
 	<tr ng-repeat="item in shoppingList">
-		<td class="item-{{item.isGotten}}">{{item.text}}</td>
-		<td class="gottenButton-{{item.isGotten}}">
-			<img src='images/toggleGotten-{{item.isGotten}}.png' class='button'>
+		<td class="item-{{item.isGotten}}">
+			{{item.text}}			
 		</td>
-		<td class="delete">
+		<td class="gottenButton-{{item.isGotten}}">
+			<input type="checkbox" ng-model="item.isGotten" class="gottenCheckbox">
+		</td>
+		<td class='delete' ng-click="removeItem($index)">
 			<img src='images/remove.png' class='button'>
 		</td>
-	</td>
-</ul>
-
-
-<table id="itemsTable" class="items">
-<!--Populated by AJAX fn "retrieveShoppingList()"-->
+	</tr>
 </table>
 
-<!--Secondary table for favorites menu and "add item" entry box"-->
+<table class='items'>
+<form ng-submit='addNewItem()'>
+	<tr id='inputRow'>
+		<td class='newItemMarkerIcon'>
+			<img src='images\edit.png' class='button'>
+		</td>
+		<td class='newText'>
+			<input type="text" ng-model="newItemText" placeholder="Add new item..." id='newItem'>
+		</td>
+		<td class='addText' id='newItemCell'>
+			<img src='images\add.png' class='button'>
+		</td>
+	</tr>
+</form>
+</table>
+</div>
+
+
+<!--
+<table id="itemsTable" class="items">
+</table>
+
+
 <table class="items">
 
-<!--New item text box-->
+
 <form action="null.php" onsubmit="addItemFromNewTextBox(); return false;">
 <tr id='inputRow'>
 	<td class='newItemMarkerIcon'>
@@ -88,6 +105,7 @@ retrieveShoppingList();
 </tr>
 </form>
 </table>
+-->
 
 
 
--- shoppingListNg.js
+++ shoppingListNg.js
@@ -1,7 +1,19 @@
 function ShoppingListCtrl($scope) {
 	$scope.shoppingList = [
-		{text: 'Protein bars', isGotten: 'notGotten'},
-		{text: 'Milk', isGotten: 'notGotten'},
-		{text: 'Bananas', isGotten: 'gotten'}
+		{text: 'Protein bars', isGotten: false},
+		{text: 'Milk', isGotten: false},
+		{text: 'Bananas', isGotten: true}
 	];
+	
+	$scope.addNewItem = function() {
+		$scope.shoppingList.push({
+			text: $scope.newItemText,
+			isGotten: false
+		});
+		$scope.newItemText = '';
+	};
+	
+	$scope.removeItem = function(index) {
+		$scope.shoppingList.splice(index, 1);
+	}
 }
